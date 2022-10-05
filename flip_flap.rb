6488c49d47b7ece498d3c2515736aeba90b2287e
pride829 <pride829@gmail.com>
2022-10-05 23:53:19 +0800

    Add tsv_buddy

--- flip_flap.rb
+++ flip_flap.rb
@@ -6,6 +6,9 @@ require_relative 'yaml_buddy'
 # Converts tabular data between storage formats
 class FlipFlap
   # Do NOT create an initialize method
+  
+  include TsvBuddy
+  include YamlBuddy
 
   attr_reader :data
 
--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,38 @@
+# frozen_string_literal: true
+
+TAB = "\t"
+NEWLINE = "\n"
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
-  def take_tsv(tsv)
 
+  def split_tsv(tsv)
+    tsv.split(NEWLINE)
+  end
+
+  def split_row(row)
+    row.chomp.split(TAB)
+  end
+
+  def named_values(headers, column)
+    headers.zip(column).to_h
+  end
+
+  def take_tsv(tsv)
+    splitted_rows = split_tsv(tsv).map { |row| split_row(row) }
+    headers = splitted_rows.first
+    @data = splitted_rows.drop(1).map { |col| named_values(headers, col) }
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    columns = @data[0].keys
+    s = columns.join(TAB)
+    @data.each do |splitted_row|
+      s += splitted_row.values.join(TAB) + NEWLINE
+    end
+    s
   end
 end
